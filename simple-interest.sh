ae2f93f522063d3a9b0086512948942aaf3686b5
Sree <sreeraj_ec@hotmail.com>
2023-10-01 11:30:46 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors: 
-#sree-r-one
+# Sreeraj Edakulathil Chellappan
 # Input:
 # p, principal amount
 # t, time period in years
