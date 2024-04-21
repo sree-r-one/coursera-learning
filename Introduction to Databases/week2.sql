107fd9c570227645207731921ba5715e76eef8d0
sree-r-one <sreeraj_ec@hotmail.com>
2024-04-21 18:43:20 +0800

    commit of the day

---
+++ Introduction to Databases/week2.sql
@@ -0,0 +1,21 @@
+ALTER TABLE student ADD(
+    location VARCHAR(50),
+    nationality VARCHAR(255)
+);
+
+CREATE TABLE customers(
+    customer_id INT, 
+    customer_name VARCHAR(50),
+    customer_address VARCHAR(255)
+);
+
+INSERT INTO `customers` (`customer_ID`, `customer_Name`, `customer_Address`) VALUES
+(1, 'Jack', '115 Old street Belfast'),
+(2, 'James', '24 Carlson Rd London'),
+(4, 'Maria', '5 Fredrik Rd, Bedford'),
+(5, 'Jade', '10 Copland Ave Portsmouth '),
+(6, 'Yasmine', '15 Fredrik Rd, Bedford'),
+(3, 'Jimmy', '110 Copland Ave Portsmouth'); 
+
+ALTER DATABASE customers RENAME TO bookshop;
+RENAME DATABASE customers TO bookshop;
--- Programming with JavaScript/jsobjects.js
+++ Programming with JavaScript/jsobjects.js
@@ -5,4 +5,22 @@ var storeManager = {
   health: 30,
 };
 
+var assistantManager = {
+  movement: 4,
+  socialSkills: 50,
+  streetSkills: 50,
+  health: 30,
+};
+
 console.log(storeManager.health);
+
+var clothes = [];
+clothes.push("gray t-shirt");
+clothes.push("green scarf");
+clothes.pop();
+clothes.push("slippers");
+clothes.pop();
+clothes.push("boots");
+clothes.push("old jeans");
+
+console.log(clothes.length);
